5cd790aa401719cf320bc96754d4f04ea1b3c104
Himandri Sharma <himandrisharma@Himandris-MacBook-Pro.local>
2022-09-12 01:28:09 +0530

    completed three.js functionalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Sphere from "./components/sphere";
+
+const CanvasContainer = styled.div`
+	width: 100%;
+	height: 100%;
+`;
 
 function App() {
-  return (
-    <div className="App">
-     hi
-    </div>
-  );
+	return <CanvasContainer>
+        <Canvas>
+            <Suspense fallback={null}>
+                <Sphere />
+            </Suspense>
+        </Canvas>
+    </CanvasContainer>;
 }
 
 export default App;
---
+++ src/components/sphere/index.js
@@ -0,0 +1,56 @@
+import { useFrame, useLoader } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { TextureLoader } from "three";
+import BaseMap from "../../assets/basecolor.jpg";
+import Height from "../../assets/height.png";
+import Normal from "../../assets/normal.jpg";
+import Roughness from "../../assets/roughness.jpg";
+import Specular from "../../assets/ambientOcclusion.jpg";
+import { OrbitControls, Stars } from "@react-three/drei";
+
+function Sphere(props) {
+	const [colorMap, specularMap, normalMap] = useLoader(TextureLoader, [
+		BaseMap,
+		Specular,
+		Normal,
+	]);
+	const sphereRef = useRef();
+	useFrame(({ clock }) => {
+		const elapsedTime = clock.getElapsedTime();
+		if(sphereRef.current){ sphereRef.current.rotation.y = elapsedTime / 6};
+	});
+	return (
+		<>
+			{/* <ambientLight intensity={1} /> */}
+			<pointLight color="white" position={[2, 0, 3]} intensity={10} />
+			<Stars
+				radius={300}
+				depth={60}
+				count={40000}
+				factor={7}
+				saturation={1}
+				fade={true}
+			/>
+			<mesh ref={sphereRef} position={[0,0,1]}>
+				<sphereGeometry args={[1, 32, 32]} />
+				<meshPhongMaterial specularMap={specularMap} />
+				<meshStandardMaterial
+					map={colorMap}
+					normalMap={normalMap}
+					metalness={1}
+					roughness={0.5}
+				/>
+				{/* <OrbitControls
+					enableZoom={true}
+					enablePan={true}
+					enableRotate={true}
+					zoomSpeed={0.6}
+					panSpeed={0.5}
+					rotateSpeed={0.4}
+				/> */}
+			</mesh>
+		</>
+	);
+}
+
+export default Sphere;
